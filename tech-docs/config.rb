a660f971ef497f991ffc3fe0981a6bad4c5d4f63
Duncan Brown <duncanjbrown@gmail.com>
2022-02-18 09:56:08 +0000

    Monkey patch contents builder to support titles in frontmatter
    This is a bit horrible, but it means we can keep going. Effectively the
    govuk_tech_docs panics when there's not H1 on the page. We use this
    override to (at the last minute) inject one.

--- tech-docs/config.rb
+++ tech-docs/config.rb
@@ -1,4 +1,21 @@
+# frozen_string_literal: true
+
 require 'govuk_tech_docs'
+
+GovukTechDocs::TableOfContents::Helpers.module_eval do
+  def list_items_from_headings(html, url: '', max_level: nil)
+    headings = GovukTechDocs::TableOfContents::HeadingsBuilder.new(html, url).headings
+
+    if headings.none? { |heading| heading.size == 1 }
+      resource = sitemap.find_resource_by_path("#{url[1..]}index.html")
+      html = "<h1>#{resource.data.title}</h1>"
+      headings = GovukTechDocs::TableOfContents::HeadingsBuilder.new(html, url).headings
+    end
+
+    tree = GovukTechDocs::TableOfContents::HeadingTreeBuilder.new(headings).tree
+    GovukTechDocs::TableOfContents::HeadingTreeRenderer.new(tree, max_level: max_level).html
+  end
+end
 
 GovukTechDocs.configure(self)
 
